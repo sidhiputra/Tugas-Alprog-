2c56268442425126bbf9b08864b64ea1fbc8001f
sidhiputra <wedaputra15@gmail.com>
2024-09-16 12:24:15 +0800

    soal2.c

--- soal2.c
+++ soal2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h> 
+#include <stdlib.h>
 
 void compare_strings(const char* s1, const char* s2) {
     int len1 = strlen(s1);
     int len2 = strlen(s2);
 
-    
     if (len1 != len2) {
         printf("BERBEDA\n");
     } else {
@@ -22,12 +22,10 @@ void compare_strings(const char* s1, const char* s2) {
 int main() {
     char string1[101];
     char string2[101];
-
     
     fgets(string1, sizeof(string1), stdin);
     string1[strcspn(string1, "\n")] = '\0';
 
-    
     fgets(string2, sizeof(string2), stdin);
     string2[strcspn(string2, "\n")] = '\0';
 
